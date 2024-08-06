827e6175eece5328c9995921eac377a764033b48
mariaato <160027558+mariaato@users.noreply.github.com>
2024-08-06 10:42:27 -0300

    Update Variaveis.php

--- Variaveis.php
+++ Variaveis.php
@@ -4,6 +4,17 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <style>
+
+        *{
+            text-align: center;
+        }
+        h1{
+            color: pink;
+            background-color: black;
+        }
+
+    </style>
 </head>
 <body>
     
@@ -11,15 +22,18 @@
 //Variaveis de número e texto
 $idade = 18;
 $nome = "Maria";
-echo "<h1>Idade: " . $idade . "</h1><br>"; 
+echo "<h1>Idade:</h1> " . $idade . "<br>"; 
 
-echo "Nome: " . $nome;
+echo "<h1>Nome: </h1>" . $nome;
 // operador ternário
+echo "<h1>Operador ternário</h1>";
 $status = ($idade >= 18) ? "Maior de idade" : "Menor de idade";
-echo  "<br> <h2>" . $status . "</h2>" ;
+echo  "<h2>" . $status . "</h2>" ;
 
 
 // Definindo duas variaveis booleanas 
+
+echo "<h1>Boolean</h1>";
 $temConta = true;
 $possuiCartao = false; 
 
@@ -28,6 +42,7 @@ echo "Tem conta: " . ($temConta ? "Sim " : "não " . "<br>");
 echo "<br>Possui cartão? " . ($possuiCartao ? "Sim" : "Não");
 
 //Array
+echo "<h1>Array</h1>";
 $cores = array("vermelho" ,"verde", "azul");
 $numeros = [1, 2, 3, 4, 5];
 
